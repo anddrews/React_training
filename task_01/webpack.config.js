b4e88929deb89ad5d4448bb95ffae986f7691905
Kondratenko Andrey <anddrews@tut.by>
2017-03-06 23:56:09 +0300

    little change in webpack.config

--- task_01/webpack.config.js
+++ task_01/webpack.config.js
@@ -1,11 +1,10 @@
-var debug = process.env.NODE_ENV !== "production";
 var webpack = require('webpack');
 var path = require('path');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   context: path.join(__dirname, "src"),
-  devtool: debug ? "inline-sourcemap" : null,
+  devtool: "inline-sourcemap",
   entry: "./js/common.js",
   module: {
     loaders: [
